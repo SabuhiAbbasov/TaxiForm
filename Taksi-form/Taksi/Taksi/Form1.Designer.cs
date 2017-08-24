e15b61c56776e66d14757f676445733a685ad370
SabuhiAbbasov <sebuhiabbas90@mail.ru>
2017-08-24 10:21:45 +0400

    Forma

---
+++ Taksi-form/Taksi/Taksi/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Taksi
+{
+    partial class Taksi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Taksilər = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.x = new System.Windows.Forms.TextBox();
+            this.yClent = new System.Windows.Forms.TextBox();
+            this.taxiSİfaris = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.xClent = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.y = new System.Windows.Forms.TextBox();
+            this.nameSurname = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ClentList = new System.Windows.Forms.ComboBox();
+            this.TaxiList = new System.Windows.Forms.ComboBox();
+            this.Nomre = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Taksilər
+            // 
+            this.Taksilər.AutoSize = true;
+            this.Taksilər.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Taksilər.Location = new System.Drawing.Point(39, 13);
+            this.Taksilər.Name = "Taksilər";
+            this.Taksilər.Size = new System.Drawing.Size(54, 24);
+            this.Taksilər.TabIndex = 0;
+            this.Taksilər.Text = "Taksi";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(674, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Müştəri";
+            // 
+            // x
+            // 
+            this.x.Location = new System.Drawing.Point(79, 42);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(100, 20);
+            this.x.TabIndex = 1;
+            // 
+            // yClent
+            // 
+            this.yClent.Location = new System.Drawing.Point(693, 75);
+            this.yClent.Name = "yClent";
+            this.yClent.Size = new System.Drawing.Size(100, 20);
+            this.yClent.TabIndex = 1;
+            // 
+            // taxiSİfaris
+            // 
+            this.taxiSİfaris.Location = new System.Drawing.Point(224, 163);
+            this.taxiSİfaris.Name = "taxiSİfaris";
+            this.taxiSİfaris.Size = new System.Drawing.Size(441, 20);
+            this.taxiSİfaris.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "x";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(6, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Nömrəsi";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(639, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "x";
+            // 
+            // xClent
+            // 
+            this.xClent.Location = new System.Drawing.Point(693, 49);
+            this.xClent.Name = "xClent";
+            this.xClent.Size = new System.Drawing.Size(100, 20);
+            this.xClent.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(639, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "y";
+            // 
+            // y
+            // 
+            this.y.Location = new System.Drawing.Point(79, 68);
+            this.y.Name = "y";
+            this.y.Size = new System.Drawing.Size(100, 20);
+            this.y.TabIndex = 1;
+            // 
+            // nameSurname
+            // 
+            this.nameSurname.Location = new System.Drawing.Point(693, 101);
+            this.nameSurname.Name = "nameSurname";
+            this.nameSurname.Size = new System.Drawing.Size(148, 20);
+            this.nameSurname.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(609, 101);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 20);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Ad,Soyad";
+            this.label8.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Taksi çağır";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(718, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Əlavə et";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ClentList
+            // 
+            this.ClentList.FormattingEnabled = true;
+            this.ClentList.Location = new System.Drawing.Point(361, 45);
+            this.ClentList.Name = "ClentList";
+            this.ClentList.Size = new System.Drawing.Size(121, 21);
+            this.ClentList.TabIndex = 4;
+            // 
+            // TaxiList
+            // 
+            this.TaxiList.FormattingEnabled = true;
+            this.TaxiList.Location = new System.Drawing.Point(361, 75);
+            this.TaxiList.Name = "TaxiList";
+            this.TaxiList.Size = new System.Drawing.Size(121, 21);
+            this.TaxiList.TabIndex = 4;
+            // 
+            // Nomre
+            // 
+            this.Nomre.Location = new System.Drawing.Point(79, 98);
+            this.Nomre.Name = "Nomre";
+            this.Nomre.Size = new System.Drawing.Size(148, 20);
+            this.Nomre.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(114, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Əlavə et";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(280, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Müştərilər";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(280, 75);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(63, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Taksilər";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label10.Location = new System.Drawing.Point(89, 163);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(117, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Sifarişin qəbulu";
+            // 
+            // Taksi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 454);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Nomre);
+            this.Controls.Add(this.TaxiList);
+            this.Controls.Add(this.ClentList);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameSurname);
+            this.Controls.Add(this.taxiSİfaris);
+            this.Controls.Add(this.y);
+            this.Controls.Add(this.yClent);
+            this.Controls.Add(this.xClent);
+            this.Controls.Add(this.x);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Taksilər);
+            this.Name = "Taksi";
+            this.Text = "Taksi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Taksilər;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox x;
+        private System.Windows.Forms.TextBox yClent;
+        private System.Windows.Forms.TextBox taxiSİfaris;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox xClent;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox y;
+        private System.Windows.Forms.TextBox nameSurname;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox ClentList;
+        private System.Windows.Forms.ComboBox TaxiList;
+        private System.Windows.Forms.TextBox Nomre;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Taksi-form/Taksi/Taksi/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taksi
+{
+    public partial class Taksi : Form
+    {
+        public Taksi()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TaxiList.Items.Add(Nomre.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClentList.Items.Add(nameSurname.Text);  
+        }
+    }
+}
